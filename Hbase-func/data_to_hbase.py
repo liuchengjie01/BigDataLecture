a0b68c79b3dfda245d01881f90f320ddf387b343
tansy <1365519162@qq.com>
2020-12-25 16:49:22 +0800

    已经可以插入数据，但是rowkey需要再调整一下

--- Hbase-func/data_to_hbase.py
+++ Hbase-func/data_to_hbase.py
@@ -7,6 +7,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 import csv
 import happybase
+import json
 from itertools import islice
 
 data_path = ''
@@ -21,17 +22,17 @@ data = {
     'cf1:user_name': '',
     'cf1:content': '',
     # 微博中头条文章的url,可能为空
-    'cf1:article_url': '',
-    'cf1:src_place': '',
+    'cf1:headline_url': '',
+    'cf1:publish_place': '',
     'cf1:aite_user': '',
     'cf1:topic': '',
-    'cf1:forward_num': 0,
-    'cf1:comment_num': 0,
-    'cf1:like_nums': 0,
+    'cf1:forward_num': '',
+    'cf1:comment_num': '',
+    'cf1:like_nums': '',
     'cf1:publish_time': '',
     'cf1:publish_tool': '',
-    'cf1:pic_urls': [],
-    'cf1:video_urls': [],
+    'cf1:pic_urls': '[]',
+    'cf1:video_urls': '[]',
     'cf1:retweet_id': ''
 }
 
@@ -42,40 +43,41 @@ def get_csv_path():
 
 
 def list_to_dict(line):
+    # 使用hbase数据的时候，需要取出来，然后json.loads一下
     if line[0] != '':
-        data['cf1:id'] = line[0]
+        data['cf1:id'] = json.dumps(line[0])
     if line[1] != '':
-        data['cf1:bid'] = line[1]
+        data['cf1:bid'] = json.dumps(line[1])
     if line[2] != '':
-        data['cf1:user_id'] = line[2]
+        data['cf1:user_id'] = json.dumps(line[2])
     if line[3] != '':
-        data['cf1:user_name'] = line[3]
+        data['cf1:user_name'] = json.dumps(line[3])
     if line[4] != '':
-        data['cf1:content'] = line[4]
+        data['cf1:content'] = json.dumps(line[4])
     if line[5] != '':
-        data['cf1:article_url'] = line[5]
+        data['cf1:headline_url'] = json.dumps(line[5])
     if line[6] != '':
-        data['cf1:src_place'] = line[6]
+        data['cf1:publish_place'] = json.dumps(line[6])
     if line[7] != '':
-        data['cf1:aite_user'] = line[7]
+        data['cf1:aite_user'] = json.dumps(line[7])
     if line[8] != '':
-        data['cf1:topic'] = line[8]
+        data['cf1:topic'] = json.dumps(line[8])
     if line[9] != '':
-        data['cf1:forward_num'] = line[9]
+        data['cf1:forward_num'] = json.dumps(line[9])
     if line[10] != '':
-        data['cf1:comment_num'] = line[10]
+        data['cf1:comment_num'] = json.dumps(line[10])
     if line[11] != '':
-        data['cf1:like_nums'] = line[11]
+        data['cf1:like_nums'] = json.dumps(line[11])
     if line[12] != '':
-        data['cf1:publish_time'] = line[12]
+        data['cf1:publish_time'] = json.dumps(line[12])
     if line[13] != '':
-        data['cf1:publish_tool'] = line[13]
+        data['cf1:publish_tool'] = json.dumps(line[13])
     if line[14] != '':
-        data['cf1:pic_urls'] = line[14]
+        data['cf1:pic_urls'] = json.dumps(line[14])
     if line[15] != '':
-        data['cf1:video_urls'] = line[15]
+        data['cf1:video_urls'] = json.dumps(line[15])
     if line[16] != '':
-        data['cf1:retweet_id'] = line[16]
+        data['cf1:retweet_id'] = json.dumps(line[16])
 
 
 if __name__ == '__main__':
@@ -97,22 +99,27 @@ if __name__ == '__main__':
     f = csv.reader(open(get_csv_path(), 'r'))
     # 跳过表头
     for line in islice(f,1,None):
-        print("type=%s" % (type(line)))
         # 2. 将拿到的list转换为dict形式
         list_to_dict(line)
         for (key,value) in data.items():
             print("key=%s value=%s" % (key,value))
+        try:
+            table.put("2", data)
+        except Exception as e:
+            print("error:%s" % e)
+        else:
+            print("successful wirte")
         break
 
     # 如果插入数据的rowkey一样会怎样？后面的更新前面的
     # 插入数据必须是字符串
     # 可以一次性插入多个列
-    try:
-        table.put("2", {"cf1:id": "888", "cf1:name": "hahah","cf1:url":"sdasdsad"})
-    except Exception as e:
-        print("error:%s" % e)
-    else:
-        print("successful wirte")
+    # try:
+    #     table.put("2", {"cf1:id": "888", "cf1:name": "hahah","cf1:url":"sdasdsad"})
+    # except Exception as e:
+    #     print("error:%s" % e)
+    # else:
+    #     print("successful wirte")
     row = table.row("2")
     print(row)
 
